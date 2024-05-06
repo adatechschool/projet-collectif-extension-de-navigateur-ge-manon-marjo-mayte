00294715b68773dc3862df45183e893381d4ec16
geraldinegautier <gvercherand@gmail.com>
2024-05-06 10:03:37 +0200

    add popup and button

--- script.js
+++ script.js
@@ -5,22 +5,48 @@ async function searchWord(word) {
 
     // const wordl = data[0].word;
     // const phonetics = data[0].phonetics[0].text;
-    const definition = data[0].meanings[0].definitions[0].definition;
-    // const audioUrl = data[0].phonetics[1].audio[0];
+    let definition
+    if (data[0].meanings[0].definitions.length >= 1 && data[0].meanings[0].definitions[0].definition) {
+        definition = data[0].meanings[0].definitions[0].definition;
+    } else {
+        console.log("Définition non disponible pour", word);
+        alert(`Définition non disponible pour ${word}`);
+    }
 
-    // console.log("Word:", word);
+    let audioUrl;
+    if (data[0].phonetics.length >= 1 && data[0].phonetics[0].audio) {
+        audioUrl = data[0].phonetics[0].audio;
+    } else {
+        console.log("Audio non disponible pour", word);
+        alert(`Audio non disponible pour ${word}`);
+    }
+
+    console.log("Word:", word);
     // console.log("Text:", phonetics);
     console.log("Définition:", definition);
-    // console.log("Audio:", audioUrl);
+    console.log("Audio:", audioUrl);
 
-    displayWord(word, definition);
+    displayWord(word, definition, audioUrl);
 }
 
-function displayWord(word, definition) {
-    // let wordElement = document.querySelector("#searchWord");
-    // wordElement.innerHTML = definition;
-    // selectedWord(definition);
-    alert(`Définition de ${word} : ${definition}`)
+function displayWord(word, definition, audioUrl) {
+    let newWin = window.open("about:blank", "popUpName", "width=400,height=400");
+    // Écriture du contenu de la définition dans la fenêtre pop-up
+    newWin.document.write(`Définition de ${word} : ${definition}`);
+
+    // Création d'un bouton pour écouter la prononciation
+    let button = newWin.document.createElement('button');
+    button.textContent = 'Listen to the pronunciation';
+    button.addEventListener('click', function () {
+        // Création de l'élément audio
+        let audio = newWin.document.createElement('audio');
+        audio.src = audioUrl;
+        audio.controls = true; // Ajoute les contrôles de lecture
+        newWin.document.body.appendChild(audio);
+    });
+
+    // Ajout du bouton au document de la fenêtre pop-up
+    newWin.document.body.appendChild(button);
 }
 
 document.addEventListener('mouseup', function () {
